f3668d329bca653dc020fefb70455a1c163d8032
drestrepoatfluid <drestrepo@fluidattacks.com>
2022-12-06 08:23:36 -0500

    feat: change password

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -20,11 +20,11 @@
 
   users.users = {
     root = {
-      hashedPassword = "$6$NKlUfhjaPNhm263V$GGMcRBxRBIPvTZ9JVAL90a0AvrdnBQbzo6XLERrX1.QxGPbhC7TlcA5Bfqh6k3TsLkd/OpcPplO19o8J6AG7t/";
+      hashedPassword = "$6$K.LcVGhhOm6tIwK6$73yska.1TGbYr52WU7JdW8z/LDFPj5RF7HWdBjsRe2xfbVML5nTjA4j/9Dtw98LnmNwYzcKaLZ0qxe/hy7JrY.";
     };
     nixos = {
       isNormalUser = true;
-      hashedPassword = "$6$Cmzt0OYRh1Ra9nCG$52hnMtWek1aWFbCN44T9AmZrFVws4Lus7vvOObgnJWpCtIJs.v0mUYW/3a4SFm3kOxJbbV/xaYoAu9S9Wc8lb0";
+      hashedPassword = "$6$K.LcVGhhOm6tIwK6$73yska.1TGbYr52WU7JdW8z/LDFPj5RF7HWdBjsRe2xfbVML5nTjA4j/9Dtw98LnmNwYzcKaLZ0qxe/hy7JrY.";
       description = "Daniel Salazar";
       home = "/home/nixos";
       group = "users";
